7f7128ca4c11531cae7ac074e4adb7e1b9143937
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-08-05 01:01:21 -0300

    Agrego modificaciones de imagenes

--- src/components/products/ItemListContainer/ItemListContainer.jsx
+++ src/components/products/ItemListContainer/ItemListContainer.jsx
@@ -4,9 +4,11 @@ import pedidos2 from '../../../assets/images/pedidos/2.jpg';
 import pedidos3 from '../../../assets/images/pedidos/3.jpg';
 import tasas1 from '../../../assets/images/tasas/1.jpg';
 import tasas2 from '../../../assets/images/tasas/2.jpg';
+import tasas3 from '../../../assets/images/tasas/3.jpg';
 import remeras1 from '../../../assets/images/remeras/1.jpg';
 import remeras2 from '../../../assets/images/remeras/2.jpg';
 import remeras3 from '../../../assets/images/remeras/3.jpg';
+import './ItemListContainer.scss';
 
 const ItemListContainer = ({ saludo }) => {
   return (
@@ -23,6 +25,7 @@ const ItemListContainer = ({ saludo }) => {
       <div className="fila fila-2">
         <img src={tasas1} alt="Tasas 1" />
         <img src={tasas2} alt="Tasas 2" />
+        <img src={tasas3} alt="Tasas 3" />
       </div>
 
       <div className="fila fila-3">
